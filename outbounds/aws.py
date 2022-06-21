aefc63299e9cd100244539bf75ed5672c259c76f
jihyeon001 <jihyeon001@naver.com>
2022-06-21 17:07:25 +0900

    refactor: aws.py

--- outbounds/aws.py
+++ outbounds/aws.py
@@ -12,21 +12,24 @@ class AwsS3Config:
 
     def __init__(self, path:str) -> None:
         config_file = json.load(open(path, mode='r', encoding='utf8'))
-        self.service = 's3'
-        self.access_id = config_file['ACCESS_ID']
-        self.access_secret = config_file['ACCESS_SECRET']
-        self.bucket_name = config_file['BUCKET_NAME']
-        self.address = config_file["ADDRESS"]
+        self.service: str = 's3'
+        self.access_id: str = config_file['ACCESS_ID']
+        self.access_secret: str = config_file['ACCESS_SECRET']
+        self.bucket_name: str = config_file['BUCKET_NAME']
+        self.address: str = config_file["ADDRESS"]
 
 class AwsS3ClientAdapter:
 
     def __init__(self, config: AwsS3Config) -> None:
-        self.config = config
-        self.client = boto3.client(
+        self.config: AwsS3Config  = config
+        self.client: boto3.client = boto3.client(
             self.config.service,
             aws_access_key_id     = self.config.access_id,
             aws_secret_access_key = self.config.access_secret,
         )
+
+    def get_address(self) -> str:
+        return self.config.address
 
     def get_waiter(self, name: str = 'object_exists'):
         return self.client.get_waiter(name)
@@ -54,7 +57,7 @@ class AwsS3ClientAdapter:
         self.wait_until_exists(key=file_object.key)
 
     def delete(self, image_url) -> None:
-        key = image_url.replace(f'{self.config.address}', '')
+        key: str = image_url.replace(f'{self.config.address}', '')
         self.client.delete_object(
             Bucket = self.config.bucket_name,
             Key    = key,
